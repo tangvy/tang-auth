8a7344cf564b2ebb44adc7f994d991fc1521f1c1
tangvy <949129882@qq.com>
2020-12-10 21:10:26 +0800

    first commit

---
+++ src/main/java/com/tangv/tangvauth/TangvAuthApplication.java
@@ -0,0 +1,13 @@
+package com.tangv.tangvauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TangvAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TangvAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tangv/tangvauth/TangvAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tangv.tangvauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TangvAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
